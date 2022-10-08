6ee93d87b575d5d48f69431e2bd8b28439dbe22c
Daniel Egan <daniel.egan@fairgroundtown.co.uk>
2022-10-08 17:48:19 +0100

    Completed Exercise 1

--- Short_1/src/main.rs
+++ Short_1/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    print_first_letter();
+    print_second_letter();
+}
+
+fn print_first_letter() {
+    println!("DDD");
+    println!("D  D");
+    println!("D  D");
+    println!("D  D");
+    println!("DDD");
+}
+
+fn print_second_letter(){
+    println!("EEEE");
+    println!("EEEE");
+    println!("E");
+    println!("E");
+    println!("EEEE");
 }
